d364e6629b10485e3d34245351942f337466d2f3
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-11-19 01:21:10 -0800

    Feat: Replugged new JavaScript in and got the fetch working using jQuery, HTML has been cleaned up and boxes shifted around.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,24 @@ var searchBarEl = document.querySelector("#searchBar");
 var searchCityEl = document.querySelector("#searchCity");
 var searchBtnEl = document.querySelector("#searchButton").value;
 var locationEl = document.querySelector("#locationResult");
-var weatherDisplayEl = document.querySelector("#weatherDisplay")
+var weatherDisplayEl = document.querySelector("#weatherDisplay");
 const APIkey = '55b80c71193a286e4403adee39a3f1d3'; // Should make it easier to plug into the code
 
-// function (searchWeather(searchTerm) {
-//     fetch (
-//         'http://api.openweathermap.org/data/2.5/forecast?q=' + searchCityEl + "&appid=" + APIkey
-//     )
-// });
+// Currently hardcoded, needs to adjusted to allow user input to determine where to search
+// Try a function using an onclick event for searchBtnEl. Link below may help
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid=55b80c71193a286e4403adee39a3f1d3 
 
-// Notes: &api_key=
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Reno&units=imperial&appid=" + APIkey, function(data) {
+    
+    console.log(data);
+
+    // Get data found in main
+    var weather = "Weather Condition: " + data.weather[0].main;
+    var temp = "Temperature: " + "°" + data.main.temp;
+    var humid = "Humidity: " + data.main.humidity + "%";
+
+    // Then append data to html
+    $("#temp").append(temp);
+    $("#weather").append(weather);
+    $("#humid").append(humid);
+});
