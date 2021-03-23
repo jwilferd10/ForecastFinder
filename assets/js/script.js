af30f24aa7adad4c874931999a57f133b73d4711
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-22 20:02:28 -0700

    Feat: Overhauled the JavaScript to finally find current Weather conditions. Also removed the 5 day forecast and prior cities on the left

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,67 @@
-// ApiKey
-const apiKey = '98c449ac6da7543bd710d0a40481466f'
+// The appID should be a const that contains our apiKey
+const appID = '98c449ac6da7543bd710d0a40481466f';
+// We'll want the units to be imperial
+const units = 'imperial';
+// searchMethod will be an empty string
+const searchMethod = '';
+
+function weatherForecast(userInput) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q${searchMethod}=${userInput}&APPID=${appID}&units=${units}`)
+    .then(result => {
+      return result.json(); // Return the appropriate JSON
+    })
+    .then(result => {
+        findWeather(result); // Initialize the application
+    });
+};
+    
+// Create an event listener for the search button
+document.getElementById('searchButton').addEventListener('click', () => {
+    // Take the value of the userInput
+    let userInput = document.getElementById('searchInput').value;
+    if(userInput)
+    weatherForecast(userInput);
+});
+
+// Create a function that'll initialize the application and return the user input to the HTML
+function findWeather(resultFromServer){
+    // Importing the index.html's ID's into the function.
+    const currentConditionEl = document.getElementById('currentCondition');
+    const currentTempEl = document.getElementById('currentTemp');
+    const humidityElement = document.getElementById('humidity');
+    const windSpeedEl = document.getElementById('windSpeed');
+    const cityNameEl = document.getElementById('cityName');
+    const currentWeatherPNG = document.getElementById('currentWeatherPNG');
+    
+    // Referencing the imported ID's above, applying innerHTML that acts as the returned results
+    
+    // APPLY INNER HTML //
+    
+    //
+    let weatherData = resultFromServer.weather[0].description;
+    
+    // Take the current conditions and capitalize the first character
+    currentConditionEl.innerText = weatherData.charAt(0).toUpperCase() + weatherData.slice(1);
+
+    // Brings back the name of the searched city
+    cityNameEl.innerHTML = resultFromServer.name; 
+
+    // Present a png image of current weather conditions of searched location
+    currentWeatherPNG.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+   
+    // Brings back the current temperature of searched city
+    currentTempEl.innerHTML = 'Currently: ' + Math.floor(resultFromServer.main.temp) + ' &#176F';
+    
+    // Bring back the current wind velocity 
+    windSpeedEl.innerHTML = 'Winds: ' + Math.floor(resultFromServer.wind.speed) + ' velocity';
+
+    // Bring back the current humidity of searched city 
+    humidityElement.innerHTML = 'Humidity: ' + resultFromServer.main.humidity + '%';
+
+    // Console.log the results from the server
+    // This is to see what information is transfered from the openweatherAPI
+    console.log(resultFromServer);
+};
+    
+
+    
